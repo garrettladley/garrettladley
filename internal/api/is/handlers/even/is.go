613ec0a15be9e2e26867dbae09f38172ae1bac54
garrettladley <garrett.ladley14@gmail.com>
2024-12-04 13:05:11 -0500

    feat: hide true error from client

--- internal/api/is/handlers/even/is.go
+++ internal/api/is/handlers/even/is.go
@@ -1,17 +1,26 @@
 package even
 
 import (
+	"errors"
+	"log/slog"
 	"strconv"
 
 	"github.com/garrettladley/garrettladley/internal/api/is/predicate"
 	"github.com/garrettladley/garrettladley/pkg/xerr"
+	"github.com/garrettladley/garrettladley/pkg/xslog"
 	"github.com/gofiber/fiber/v2"
 )
 
 func (s *Service) Is(c *fiber.Ctx) error {
 	n, err := strconv.ParseInt(c.Params("n"), 10, 64)
 	if err != nil {
-		return xerr.BadRequest(err)
+		slog.LogAttrs(
+			c.Context(),
+			slog.LevelError,
+			"failed to parse integer",
+			xslog.Error(err),
+		)
+		return xerr.BadRequest(errors.New("invalid number"))
 	}
 	var result bool
 	if c.QueryBool("ai", false) {
